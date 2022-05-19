3d2d99675757b8347ad0f1f5210f9d5742c4e4fd
MasterThien <94881701+MasterThien@users.noreply.github.com>
2022-05-19 22:08:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Gửi vợ tương lai",
-    introTitle: 'Đinh T Hồng Hạnh',
+    introTitle: 'Nhii',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi bạn Thiện đẹp trai<3',
     reply: 'Thiện Thiện Thiện',
     mess: 'Tui biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Làm người iu anh nhé beby.',
+    messDesc: 'Làm người iu anh nhé babii.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/Hong.Tri.Thien ' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
